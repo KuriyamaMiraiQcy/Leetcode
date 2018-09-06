2ecd7e7c740fd795e46556044496893be510ad4b
Kuriyama <Qcy960801>
2018-09-06 13:12:46 -0500

    graph

---
+++ src/CloneGraph.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class CloneGraph {
+    HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(node.label, clone);
+        for (UndirectedGraphNode neighbor: node.neighbors) {
+            if (!map.containsKey(neighbor.label)) {
+                clone.neighbors.add(cloneGraph(neighbor));
+            } else{
+                clone.neighbors.add(map.get(neighbor.label));
+            }
+        }
+        return clone;
+    }
+}
+
+class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+};
---
+++ src/CourseSchedule.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CourseSchedule {
+    int num;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        num = numCourses;
+        HashMap<Integer, Graph> map = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new Graph(i));
+        }
+
+        for (int[] pair: prerequisites) {
+            Graph start = map.get(pair[0]);
+            start.neighbors.add(map.get(pair[1]));
+        }
+
+        return isCycle(map);
+    }
+
+    private boolean isCycle(HashMap<Integer, Graph> map) {
+
+        int[] visited = new int[num];
+        int[] recStack = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            if (DFS(map.get(i), visited, recStack)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean DFS(Graph node, int[] visited, int[] rec) {
+        int label = node.label;
+        if (rec[label] == 1) {
+            return true;
+        }
+        if (visited[label] == 1) {
+            return false;
+        }
+
+        visited[label] = 1;
+        rec[label] = 1;
+        for (Graph child: node.neighbors) {
+            if (DFS(child, visited, rec)) {
+                return true;
+            }
+        }
+        rec[label] = 0;
+        return false;
+    }
+
+    public class Graph{
+        int label;
+        List<Graph> neighbors;
+
+        public Graph(int label) {
+            this.label = label;
+            neighbors = new LinkedList<>();
+        }
+    }
+}
