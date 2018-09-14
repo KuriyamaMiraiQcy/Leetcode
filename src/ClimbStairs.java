49f4ee4ce8e3c21bbb3ea8286fb2d2b0fc97d6a1
Kuriyama <Qcy960801>
2018-09-13 21:48:27 -0500

    ClimbStairs

---
+++ src/ClimbStairs.java
@@ -0,0 +1,11 @@
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        int[] memory = new int[n + 1];
+        memory[0] = 0;
+        memory[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            memory[i] = memory[i-1] + memory[i-2];
+        }
+        return memory[n];
+    }
+}
