081529d81d3c28b1a3c0708fc5004686974189cb
KuriyamaMiraiQcy <623393731@qq.com>
2018-09-20 16:16:16 -0500

    Word break

---
+++ src/WordBreak.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s.length() == 0) {
+            return false;
+        }
+        int[] memory = new int[s.length()];
+
+        for (int i = s.length() -1; i >= 0; i--) {
+            if (wordDict.contains(s.substring(i))) {
+                memory[i] = 1;
+            } else {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (memory[j] != 0 && wordDict.contains(s.substring(i, j))) {
+                        memory[i] = memory[j] + 1;
+                    }
+                }
+            }
+        }
+
+        return memory[0] != 0;
+    }
+}
