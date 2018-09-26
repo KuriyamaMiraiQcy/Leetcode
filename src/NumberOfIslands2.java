254952eeebaa18cf3aff2eb1b7caf63bd8b6b8f4
Kuriyama <Qcy960801>
2018-09-26 17:24:31 -0500

    DistinctSubsequence

---
+++ src/NumberOfIslands2.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NumberOfIslands2 {
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        int[][] directions = new int[][]{{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
+        HashMap<String, String> island = new HashMap<>();
+        int count = 0;
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int[] pos : positions) {
+            String p = getPosition(pos);
+            island.put(p, p);
+            count++;
+
+            for (int[] direction : directions) {
+                int[] direct = new int[2];
+                direct[0] = pos[0] + direction[0];
+                direct[1] = pos[1] + direction[1];
+                if (direct[0] < 0 || direct[1] < 0 || direct[0] >= m || direct[1] >= n) {
+                    continue;
+                }
+                String neighbor = getPosition(direct);
+
+                if (island.keySet().contains(neighbor)) {
+                    while (!island.get(neighbor).equals(neighbor)) {
+                        neighbor = island.get(neighbor);
+                    }
+                    if (!neighbor.equals(island.get(p))) {
+                        count--;
+                        island.put(island.get(p), neighbor);
+                    }
+                }
+            }
+            list.add(count);
+        }
+        return list;
+    }
+
+    private String getPosition(int[] pos) {
+        return Integer.toString(pos[0]) + Integer.toString(pos[1]);
+    }
+
+    public static void main(String[] args) {
+        NumberOfIslands2 a = new NumberOfIslands2();
+        a.numIslands2(3, 3, new int[][]{{0, 1}, {1, 2}, {2, 1}, {1, 0}, {0, 2}, {0, 0}, {1, 1}});
+    }
+}
