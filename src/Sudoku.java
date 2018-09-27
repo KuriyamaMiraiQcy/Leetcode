2e033951a02a6cd1e20ed2da53da58ec2eb6f46a
KuriyamaMiraiQcy <623393731@qq.com>
2018-09-26 23:29:04 -0500

    Sudoku

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Sudoku {
+    public boolean isValidSudoku(char[][] board) {
+        HashMap<String, HashSet<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+            }
+        }
+    }
 }
