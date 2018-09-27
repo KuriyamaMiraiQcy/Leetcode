d9cf8f0b574a508771a65f93cfa868744fe2a1d9
Kuriyama <Qcy960801>
2018-09-27 00:40:30 -0500

    Sudoku

--- src/FractionToRecurringDecimal.java
+++ src/FractionToRecurringDecimal.java
@@ -3,20 +3,48 @@ import java.util.HashSet;
 
 public class FractionToRecurringDecimal {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator % denominator == 0) {
-            return Integer.toString(numerator / denominator);
+        long numer = (long) numerator;
+        long denom = (long) denominator;
+        StringBuilder res = new StringBuilder();
+        if ((numer > 0 && denom < 0) || (numer < 0 && denom > 0)) {
+            res.append("-");
+        }
+        numer = Math.abs(numer);
+        denom = Math.abs(denom);
+        if (numer % denom == 0) {
+            res.append(numer / denom);
+            return res.toString();
         }
 
-        StringBuilder res = new StringBuilder();
-        res.append(numerator / denominator);
+
+
+
+        res.append(numer / denom);
         res.append(".");
-        int remainder = numerator % denominator * 10;
-        HashMap<Integer, Integer> map = new HashMap<>();
+
+        long remainder = numer % denom * 10;
+        int nextIndex = res.length();
+        HashMap<Long, Integer> map = new HashMap<>();
 
         while (remainder != 0) {
-
+            if (map.containsKey(remainder)) {
+                res.append(")");
+                int index = map.get(remainder);
+                res.insert(index, "(");
+                break;
+            }
+            long div = remainder / denom;
+            res.append(div);
+            map.put(remainder, nextIndex);
+            remainder = remainder % denom * 10;
+            nextIndex++;
         }
 
+        return res.toString();
+    }
 
+    public static void main(String[] args) {
+        FractionToRecurringDecimal a= new FractionToRecurringDecimal();
+        System.out.print(a.fractionToDecimal(-2147483648, 1));
     }
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -7,8 +7,38 @@ public class Sudoku {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != '.') {
+                    int num = Character.getNumericValue(board[i][j]);
+                    String row = "Row:" + Integer.toString(i);
+                    String col = "Col:" + Integer.toString(j);
+                    String block = "Block:" + Integer.toString(i / 3) + Integer.toString(j / 3);
 
+                    if (!check(row, map, num)) {
+                        return false;
+                    }
+                    if (!check(col, map, num)) {
+                        return false;
+                    }
+                    if (!check(block, map, num)) {
+                        return false;
+                    }
+                }
             }
         }
+        return true;
+    }
+
+    private boolean check(String str, HashMap<String, HashSet<Integer>> map, int n) {
+        if (map.containsKey(str)) {
+            if (map.get(str).contains(n)) {
+                return false;
+            }
+            map.get(str).add(n);
+        } else {
+            HashSet<Integer> set = new HashSet<>();
+            set.add(n);
+            map.put(str, set);
+        }
+        return true;
     }
 }
