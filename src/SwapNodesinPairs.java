b9b6402222be5d0b943eac5286eeeca66c1e017f
Kuriyama <Qcy960801>
2018-10-08 09:08:40 -0500

    LinkedList

---
+++ src/SwapNodesinPairs.java
@@ -0,0 +1,23 @@
+public class SwapNodesinPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode pseHead = new ListNode(0);
+        ListNode prevTail = pseHead;
+        ListNode cur = head;
+
+        while (cur != null && cur.next != null) {
+            ListNode next = cur.next.next;
+            prevTail.next = cur.next;
+            cur.next = null;
+            prevTail.next.next = cur;
+            cur = next;
+            prevTail = prevTail.next.next;
+        }
+        if (cur != null) {
+            prevTail.next = cur;
+        }
+        return pseHead.next;
+    }
+}
