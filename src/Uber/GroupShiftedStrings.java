499de9e3ba9172efabfb767edc921b2dacf8aced
Kuriyama <623393731@qq.com>
2018-11-05 13:19:16 -0600

    DistinctSubsequence

--- src/Uber/GroupShiftedStrings.java
+++ src/Uber/GroupShiftedStrings.java
@@ -11,26 +11,22 @@ public class GroupShiftedStrings {
         }
 
         HashMap<String, List<String>> patterns = new HashMap<>();
-        List<String> singleOne = new ArrayList<>();
 
-        for (String s:strings) {
-            if (s.length() == 1) {
-                singleOne.add(s);
-            } else {
-                String str = "";
-                for (int i = 1; i < s.length(); i++) {
-                    str += (s.charAt(i) - s.charAt(i - 1) + 26) % 26 + ",";
-                }
-                if (!patterns.containsKey(str)) {
-                    patterns.put(str, new ArrayList<>());
-                }
-                patterns.get(str).add(s);
+
+        for (String s : strings) {
+
+            String str = "";
+            for (int i = 1; i < s.length(); i++) {
+                str += (s.charAt(i) - s.charAt(i - 1) + 26) % 26 + ",";
             }
+            if (!patterns.containsKey(str)) {
+                patterns.put(str, new ArrayList<>());
+            }
+            patterns.get(str).add(s);
+
         }
         res.addAll(patterns.values());
-        if (!singleOne.isEmpty()) {
-            res.add(singleOne);
-        }
+
         return res;
     }
 
@@ -41,17 +37,17 @@ public class GroupShiftedStrings {
         //Then subtract its first character for every character
         //eg. "abc" -> "0,1,2,"        "am"->"0,12,"
 
-        Map<String,List<String>> map = new HashMap<>();
-        for(String str : strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
             String key = "";
             char first = str.charAt(0);
-            for(char c:str.toCharArray())
-                key+=(c-first+26)%26+",";
-            if(!map.containsKey(key))
-                map.put(key,new ArrayList<String>());
+            for (char c : str.toCharArray())
+                key += (c - first + 26) % 26 + ",";
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<String>());
             map.get(key).add(str);
         }
-        for(String key:map.keySet())
+        for (String key : map.keySet())
             Collections.sort(map.get(key));
         return new ArrayList<List<String>>(map.values());
     }
--- src/Uber/RandomPickwithBlacklist.java
+++ src/Uber/RandomPickwithBlacklist.java
@@ -1,3 +1,5 @@
+package Uber;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
