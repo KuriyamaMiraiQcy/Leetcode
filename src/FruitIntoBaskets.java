7f640a7641094af1f1eeb5b2fa64c2a7c8d4ae3c
Kuriyama <623393731@qq.com>
2019-01-13 21:57:46 -0600

    fruit

--- src/FruitIntoBaskets.java
+++ src/FruitIntoBaskets.java
@@ -3,45 +3,17 @@ import java.util.HashSet;
 
 public class FruitIntoBaskets {
     public int totalFruit(int[] tree) {
-        if (tree.length < 3) {
-            return tree.length;
-        }
-
-        int end = 2;
-        int maxLength = 2;
-        int first = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(tree[0], 0);
-        map.put(tree[1], 1);
-
-        while (end < tree.length) {
-            if (map.containsKey(tree[end])) {
-                map.put(tree[end], end);
-            } else {
-                if (map.size() < 2) {
-                    map.put(tree[end], end);
-                } else {
-                    maxLength = Math.max(maxLength, end - first);
-                    int lastOccurance = Integer.MAX_VALUE;
-                    for (Integer n:map.keySet()) {
-                        lastOccurance = Math.min(lastOccurance, map.get(n));
-                    }
-                    map.remove(lastOccurance);
-                    for (Integer n:map.keySet()) {
-                        first = map.get(n);
-                    }
-                    map.put(tree[end], end);
-                }
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        int res = 0, i = 0;
+        for (int j = 0; j < tree.length; ++j) {
+            count.put(tree[j], count.getOrDefault(tree[j], 0) + 1);
+            while (count.size() > 2) {
+                count.put(tree[i], count.get(tree[i]) - 1);
+                if (count.get(tree[i]) == 0) count.remove(tree[i]);
+                i++;
             }
-
-            end++;
+            res = Math.max(res, j - i + 1);
         }
-
-        return Math.max(maxLength, end - first);
-    }
-
-    public static void main(String[] args) {
-        FruitIntoBaskets a = new FruitIntoBaskets();
-        System.out.println(a.totalFruit(new int[]{1,0,1,4,1,4,1,2,3}));
+        return res;
     }
 }
