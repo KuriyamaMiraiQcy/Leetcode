5ac927fbb1489c8fabe82c0182a193224b9f0764
cq5 <cq5@illinois.edu>
2019-02-02 18:51:22 -0600

    ACM task1

--- src/SubarraySumEqualsK.java
+++ src/SubarraySumEqualsK.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        return 0;
+        int sum = 0, result = 0;
+
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
     }
 }
