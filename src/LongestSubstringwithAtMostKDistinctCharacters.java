4d549991c1bbb96ff6479fcd43c76a28008b692f
cq5 <cq5@illinois.edu>
2019-02-07 17:27:39 -0600

    k distinct

--- src/LongestSubstringwithAtMostKDistinctCharacters.java
+++ src/LongestSubstringwithAtMostKDistinctCharacters.java
@@ -3,8 +3,17 @@ import java.util.LinkedList;
 
 public class LongestSubstringwithAtMostKDistinctCharacters {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        HashMap<Character, LinkedList<Integer>> map = new HashMap<>();
+        int[] count = new int[256];
+        int num = 0, i = 0, res = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (count[s.charAt(j)]++ == 0) num++;
+            if (num > k) {
+                while (--count[s.charAt(i++)] > 0) ;
+                num--;
+            }
+            res = Math.max(res, j - i + 1);
+        }
+        return res;
 
-        return 0;
     }
 }
