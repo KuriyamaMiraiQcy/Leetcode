9981d7b7c7479a2cfc57a9ebbf361f20d870033c
Kuriyama <623393731@qq.com>
2019-02-07 23:34:47 -0600

    oa2

--- src/PageRenderTime.java
+++
@@ -1,9 +0,0 @@
-public class PageRenderTime {
-    public int pageId;
-    public double renderTime;
-
-    public PageRenderTime(int id, double value) {
-        pageId = id;
-        renderTime = value;
-    }
-}
