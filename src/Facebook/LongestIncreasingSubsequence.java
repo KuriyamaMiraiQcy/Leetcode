b87358dd75ce281d710d02364f920f804147a515
KuriyamaMiraiQcy <623393731@qq.com>
2024-11-04 08:29:21 +0800

    commit

--- src/Facebook/LongestIncreasingSubsequence.java
+++ src/Facebook/LongestIncreasingSubsequence.java
@@ -2,6 +2,25 @@ package Facebook;
 
 public class LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
-        
+        int[] insert = new int[nums.length];
+        int end = 0;
+        insert[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > insert[end]) {
+                insert[++end] = nums[i];
+            } else {
+                int s = 0, e = end;
+                while (s < e) {
+                    int mid = (e + s) / 2;
+                    if (insert[mid] < nums[i]) {
+                        s = mid + 1;
+                    } else {
+                        e = mid;
+                    }
+                }
+                insert[s] = nums[i];
+            }
+        }
+        return end + 1;
     }
 }
