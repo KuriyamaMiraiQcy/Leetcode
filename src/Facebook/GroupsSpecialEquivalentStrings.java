56214bc31e39af66081266cae21e0f168f615c24
KuriyamaMiraiQcy <623393731@qq.com>
2024-11-15 17:25:56 +0800

    commit

--- src/Facebook/GroupsSpecialEquivalentStrings.java
+++ src/Facebook/GroupsSpecialEquivalentStrings.java
@@ -1,7 +1,18 @@
 package Facebook;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class GroupsSpecialEquivalentStrings {
     public int numSpecialEquivGroups(String[] words) {
-        
+        HashSet<String> set = new HashSet<>();
+        for (String word : words) {
+            int[] count = new int[52];
+            for (int i = 0; i < word.length(); i++) {
+                count[word.charAt(i) - 'a' + 26 * (i % 2)]++;
+            }
+            set.add(Arrays.toString(count));
+        }
+        return set.size();
     }
 }
